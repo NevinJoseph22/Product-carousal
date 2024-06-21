8ec5007f93f006f7dbb63d7b8802130d21d6042b
NevinJoseph22 <nevinjoseph2001@gmail.com>
2024-06-21 09:43:29 +0530

    product-initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CarouselComponent } from './ui/carousel/carousel.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CarouselComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'product_display';
+  title = 'productcarousel';
+
 }
